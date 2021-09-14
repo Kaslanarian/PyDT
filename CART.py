477569c6ac1bdbb738b10413a533d6754f13a364
Welt Xing <1753057600@qq.com>
2021-09-14 09:28:31 +0800

    Update CART.py

--- CART.py
+++ CART.py
@@ -9,10 +9,12 @@ class CARTNode(C4_5Node):
         value=None,
         depth=0,
         continuous=False,
+        parent=None,
     ) -> None:
         super().__init__(split_attr=split_attr,
                          value=value,
                          depth=depth,
+                         parent=parent,
                          continuous=continuous)
 
 
@@ -27,36 +29,39 @@ class CARTClassifier(C4_5Classifier):
 
     def fit(self, X, y):
         # 确定各列属性是连续还是离散
-        self.continue_attr = [C4_5Classifier.is_number(x) for x in X[0]]
+        self.continue_attr = [CARTClassifier.is_number(x) for x in X[0]]
         self.X, self.y = np.array(X), np.array(y)
-        # self.X的类型不再是数字，而是Object
+
+        self.leaves_list = []
         self.n_features = self.X.shape[1]
-        self.leaf_list = []
+
         if self.min_samples_split < 1:
             self.min_samples_split *= len(self.X)
         if self.min_samples_leaf < 1:
             self.min_samples_leaf *= len(self.X)
+
         self.root = CARTNode()
-        stack = [(
-            self.root,
-            np.arange(len(self.X)),
-            list(range(self.n_features)),
-        )]
+        self.root.sample_list = np.arange(len(self.X))
+        stack = [(self.root, list(range(self.n_features)))]
+
         while len(stack) > 0:
-            node, id_list, attr_set = stack.pop()
-            node_X, node_y = self.X[id_list], self.y[id_list]
+            node, attr_set = stack.pop()
+            node_X, node_y = self.X[node.sample_list], self.y[node.sample_list]
             unique, counts = np.unique(node_y, return_counts=True)
             prior = unique[np.argmax(counts)]
+
             if len(unique, ) == 1 or len(
                     attr_set,
             ) == 0 or len(np.unique(node_X[:, attr_set], axis=0)) == 1 or len(
-                    id_list
+                    node.sample_list
             ) <= self.min_samples_split or node.depth >= self.max_depth:
                 node.value = prior
-                self.leaf_list.append(node)
+                self.leaves_list.append(node)
             else:
-                node.split_attr, (_, node.threshold) = self.get_best_attr(
-                    id_list, attr_set)
+                node.split_attr, (_, node.threshold) = self.__get_best_attr(
+                    node.sample_list,
+                    attr_set,
+                )
                 attr_set.remove(node.split_attr)
                 if self.continue_attr[node.split_attr]:  # 连续属性
                     node.continuous = True
@@ -65,61 +70,66 @@ class CARTClassifier(C4_5Classifier):
                 else:  # 离散属性
                     index_left = node_X[:, node.split_attr] == node.threshold
                 index_right = np.logical_not(index_left)
-                left, right = id_list[index_left], id_list[index_right]
+                left, right = (
+                    node.sample_list[index_left],
+                    node.sample_list[index_right],
+                )
 
                 if min(len(left), len(right)) < self.min_samples_leaf:
                     node.value = prior
-                    self.leaf_list.append(node)
+                    self.leaves_list.append(node)
                     continue
 
-                node.children = (
-                    CARTNode(depth=node.depth + 1),
-                    CARTNode(depth=node.depth + 1),
-                )
-                stack.append(
-                    (node.children[1], id_list[index_right], attr_set.copy()))
-                stack.append(
-                    (node.children[0], id_list[index_left], attr_set.copy()))
+                left_node = CARTNode(depth=node.depth + 1, parent=node)
+                right_node = CARTNode(depth=node.depth + 1, parent=node)
+                left_node.sample_list, right_node.sample_list = left, right
+                node.children.update({True: left_node, False: right_node})
 
-        self.depth = max([leaf.depth for leaf in self.leaf_list])
-        self.n_leaf = len(self.leaf_list)
+                stack.append((node.children[False], attr_set.copy()))
+                stack.append((node.children[True], attr_set.copy()))
+
+        self.depth = max([leaf.depth for leaf in self.leaves_list])
+        self.n_leaf = len(self.leaves_list)
         return self
 
     def predict(self, X):
         X = np.array(X).reshape(-1, self.n_features)
-        stack = []
-        stack.append((self.root, np.arange(len(X))))
+        for leaf in self.leaves_list:
+            leaf.predict_list = None
+        self.root.predict_list = np.arange(len(X))
+        stack = [self.root]
+
         while len(stack) > 0:
-            node, id_list = stack.pop()
+            node = stack.pop()
             if node.value is None:
-                data = X[id_list]
+                data = X[node.predict_list]
                 if node.continuous:
                     index_left = data[:, node.split_attr].astype(
                         float) <= node.threshold
                 else:
                     index_left = data[:, node.split_attr] == node.threshold
                 index_right = np.logical_not(index_left)
-                stack.append((
-                    node.children[1],
-                    id_list[index_right],
-                ))
-                stack.append((
-                    node.children[0],
-                    id_list[index_left],
-                ))
-            else:
-                node.predict_list = id_list
-        pred = np.zeros(len(X), dtype='int')
-        for leaf in self.leaf_list:
+                node.children[True].predict_list = node.predict_list[
+                    index_left]
+                node.children[False].predict_list = node.predict_list[
+                    index_right]
+                stack.append(node.children[False])
+                stack.append(node.children[True])
+                node.predict_list = None  # 将非叶节点的predict_list还原为none
+
+        pred = np.zeros(len(X))
+        for leaf in self.leaves_list:
             if leaf.predict_list is not None:
                 pred[leaf.predict_list] = leaf.value
-                leaf.predict_list = None
         return pred
 
     def score(self, test_X, test_y):
-        return super().score(test_X, test_y)
+        X = np.array(test_X).reshape(-1, self.n_features)
+        y = np.array(test_y)
+        pred = self.predict(X)
+        return np.mean(y == pred)
 
-    def get_best_attr(self, id_list, attr_set):
+    def __get_best_attr(self, id_list, attr_set):
         X, y = self.X[id_list], self.y[id_list]
 
         attr_gini_dict = {}
@@ -176,7 +186,7 @@ class CARTRegressor(CARTClassifier):
     def __init__(
         self,
         max_depth=np.inf,
-        min_samples_split=1,
+        min_samples_split=2,
         min_samples_leaf=1,
     ) -> None:
         '''
@@ -191,38 +201,37 @@ class CARTRegressor(CARTClassifier):
         self.continue_attr = [C4_5Classifier.is_number(x) for x in X[0]]
         self.X, self.y = np.array(X), np.array(y)
         # self.X的类型不再是数字，而是Object
+
+        self.leaves_list = []
         self.n_features = self.X.shape[1]
-        self.leaf_list = []
+
         if self.min_samples_split < 1:
             self.min_samples_split *= len(self.X)
         if self.min_samples_leaf < 1:
             self.min_samples_leaf *= len(self.X)
+
         self.root = CARTNode()
-        stack = [(
-            self.root,
-            np.arange(len(self.X)),
-            list(range(self.n_features)),
-        )]
+        self.root.sample_list = np.arange(len(self.X))
+        stack = [(self.root, list(range(self.n_features)))]
+
         while len(stack) > 0:
-            node, id_list, attr_set = stack.pop()
-            node_X, node_y = self.X[id_list], self.y[id_list]
+            node, attr_set = stack.pop()
+            node_X, node_y = self.X[node.sample_list], self.y[node.sample_list]
             unique = np.unique(node_y)
             prior = np.mean(node_y)
+
             if len(unique, ) == 1 or len(
                     attr_set,
             ) == 0 or len(np.unique(node_X[:, attr_set], axis=0)) == 1 or len(
-                    id_list
+                    node.sample_list
             ) <= self.min_samples_split or node.depth >= self.max_depth:
                 node.value = prior
-                self.leaf_list.append(node)
+                self.leaves_list.append(node)
             else:
-                node.split_attr, (_, node.threshold) = self.get_best_attr(
-                    id_list,
+                node.split_attr, (_, node.threshold) = self.__get_best_attr(
+                    node.sample_list,
                     attr_set,
                 )
-                if node.threshold == np.inf:
-                    print(node.split_attr)
-                    assert 0
                 attr_set.remove(node.split_attr)
                 if self.continue_attr[node.split_attr]:  # 连续属性
                     node.continuous = True
@@ -231,22 +240,26 @@ class CARTRegressor(CARTClassifier):
                 else:  # 离散属性
                     index_left = node_X[:, node.split_attr] == node.threshold
                 index_right = np.logical_not(index_left)
-                left, right = id_list[index_left], id_list[index_right]
+                left, right = (
+                    node.sample_list[index_left],
+                    node.sample_list[index_right],
+                )
 
                 if min(len(left), len(right)) < self.min_samples_leaf:
                     node.value = prior
-                    self.leaf_list.append(node)
+                    self.leaves_list.append(node)
                     continue
 
-                node.children = (
-                    CARTNode(depth=node.depth + 1),
-                    CARTNode(depth=node.depth + 1),
-                )
-                stack.append((node.children[1], right, attr_set.copy()))
-                stack.append((node.children[0], left, attr_set.copy()))
+                left_node = CARTNode(depth=node.depth + 1, parent=node)
+                right_node = CARTNode(depth=node.depth + 1, parent=node)
+                left_node.sample_list, right_node.sample_list = left, right
+                node.children.update({True: left_node, False: right_node})
 
-        self.depth = max([leaf.depth for leaf in self.leaf_list])
-        self.n_leaf = len(self.leaf_list)
+                stack.append((node.children[False], attr_set.copy()))
+                stack.append((node.children[True], attr_set.copy()))
+
+        self.depth = max([leaf.depth for leaf in self.leaves_list])
+        self.n_leaf = len(self.leaves_list)
         return self
 
     def predict(self, X):
@@ -258,7 +271,7 @@ class CARTRegressor(CARTClassifier):
         pred = self.predict(X)
         return -np.mean((y - pred)**2)
 
-    def get_best_attr(self, id_list, attr_set):
+    def __get_best_attr(self, id_list, attr_set):
         X, y = self.X[id_list], self.y[id_list]
 
         attr_gini_dict = {}
